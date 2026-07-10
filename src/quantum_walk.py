bbaef4c33022c4b213ec193f61d663ec955f071f
leonardod16p <leonardod16p@gmail.com>
2026-07-10 14:47:21 -0300

    setting up initial state, number of qubits and start to drawing the circuit with matplotlib

--- src/quantum_walk.py
+++ src/quantum_walk.py
@@ -1,17 +1,26 @@
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator 
+import matplotlib.pyplot as plt
 
+
+#setting up the number of qubits in the circuit
+
+number_of_qubits = 8
 
 simulator = AerSimulator()  
 
+#creating a n qubit circuit and n bit string to describe the state
+circuit = QuantumCircuit(number_of_qubits, number_of_qubits)
 
-circuit = QuantumCircuit(2, 2)
+
+#our initial state is going to be  |00000001> 
+circuit.initialize('00000001', [0,1,2,3,4,5,6,7])
 
 
 circuit.h(0)
-circuit.h(1)
 
-circuit.measure([0, 1], [0, 1])
+
+circuit.measure([0,1,2,3,4,5,6,7], [0,1,2,3,4,5,6,7])
 
 
 job = simulator.run(circuit, shots=1024)
@@ -19,4 +28,6 @@ result = job.result()
 counts = result.get_counts(circuit)
 print("resultados:", counts)
 
-circuit.draw(output="mpl")
+fig = circuit.draw(output="mpl")
+
+plt.show()
